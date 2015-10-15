836b9caa7de9458f0c65d469afc0b40070157035
Jeff Campbell <jeffcampbell@meetup.com>
2015-10-15 11:57:37 -0400

    switch to group id

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ post "/" do
 end
 
 def generate_attachment
-  uri = "https://api.meetup.com/2/events?group_urlname=#{ENV["MEETUP_GROUP_URL"]}&page=1&key=#{ENV["MEETUP_API_KEY"]}"
+  uri = "https://api.meetup.com/2/events?group_id=#{ENV["MEETUP_GROUP_ID"]}&page=1&key=#{ENV["MEETUP_API_KEY"]}"
   request = HTTParty.get(uri)
   puts "[LOG] #{request.body}"
 
