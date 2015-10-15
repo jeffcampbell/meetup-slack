944ed07c4184e3d39c977acdd75b9ed312717b01
Jeff Campbell <jeffcampbell@meetup.com>
2015-10-15 12:36:41 -0400

    forgot that as well

--- app.rb
+++ app.rb
@@ -55,7 +55,7 @@ def generate_attachment
   get_firsturl = @firstresults["event_url"]
   raw_firsttime = @firstresults["time"].to_f / 1000
   utc_firstoffset = @firstresults["utc_offset"].to_i / 1000
-  calc_firsttime = Time.at(raw_firsttime).getlocal(utc_offset)
+  calc_firsttime = Time.at(raw_firsttime).getlocal(utc_firstoffset)
   cldr_firsttime = calc_firsttime.localize
   get_firsttime = "#{cldr_time.to_short_s} #{cldr_time.to_date.to_full_s}"
   end
@@ -80,7 +80,7 @@ def generate_attachment
   get_secondurl = @secondresults["event_url"]
   raw_secondtime = @secondresults["time"].to_f / 1000
   utc_secondoffset = @secondresults["utc_offset"].to_i / 1000
-  calc_secondtime = Time.at(raw_secondtime).getlocal(utc_offset)
+  calc_secondtime = Time.at(raw_secondtime).getlocal(utc_secondoffset)
   cldr_secondtime = calc_secondtime.localize
   get_secondtime = "#{cldr_time.to_short_s} #{cldr_time.to_date.to_full_s}"
 
